12ebd9e9f3207b1c901f8c0d06b06fa833c75b00
Kristen Alford <kristen@rallio.com>
2019-10-18 10:29:29 -0700

    locations code added back in

--- app/send-to-chute.js
+++ app/send-to-chute.js
@@ -1,21 +1,12 @@
-const longPoller = require('./long-poller');
-const {getTags} = require('./get-tags');
-const {getSqs} = require('./consume-liked-photos');
 const {chuteMapping} = require('./mapping');
 const {saveRequest} = require('./save-request');
-const {checkDB} = require('./check-existing');
-const {getFalse} = require('./get-false-request');
 const {sendMessage} = require('./send-message');
 const {saveSuccess} = require('./save-success');
 const {removeFromSqs} = require('./remove-from-sqs');
 
-const flat = ((a, b) => [].concat(a, b));
-const flatten = arr => arr.reduce(flat, [])
-
-async function sendToChute (message) {
-    console.log("!!!!!!message",message.tag)
-    const mappedResult = await chuteMapping(message, message.db, message.type);
-    console.log("!!!!!!mappedResult",mappedResult)
+async function sendToChute (message) { 
+    const mappedResult = await chuteMapping(message, message.db, message.pkName);
+   
     if (mappedResult.length > 0) {
       const mappedData =  {
         album: mappedResult[0].album_code,
@@ -25,11 +16,11 @@ async function sendToChute (message) {
         photo_id: message.photo_id,
         receiptHandle: message.receiptHandle,
         message_id: message.message_id,
-        type: message.tag
+        type: message.type,
+        tag:message.tag
       }
       const savedToDatabaseResult = await saveRequest(mappedData);
-      console.log("!!!!!!savedToDatabaseResult",savedToDatabaseResult)
-      
+            
       const newData = {
         album: mappedData.album,
         file_url: mappedData.file_url,
@@ -42,26 +33,26 @@ async function sendToChute (message) {
         id: savedToDatabaseResult.id
       }
       const chuteResult = await sendMessage(newData);
-      console.log("!!!!!!savedToDatabaseResult",chuteResult)
+     
       if (chuteResult !== true){
-        return reject("There was no chute result")
+        return Promise.reject("There was no chute result")
       }
       const successData =  await saveSuccess(newData);
       console.log("!!!!!1", successData)
-    //   const resultChutePromise = await Promise.all(sendToChutePromises)
-    //   const flattened = flatten(resultChutePromise)
-    //   const allGood = flattened.every(Boolean)
-  
+    
       if (!successData) {
         return Promise.reject('this stays in the queue')
       }
     
+      if(newData.type === 'location'){
       const removedResult = await removeFromSqs({
         ReceiptHandle: newData.receiptHandle
       });
       console.log("removedResult",removedResult)
       return removedResult
-
+      }else{
+        return true
+      }
     }
 }
 
