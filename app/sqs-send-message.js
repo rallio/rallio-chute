c88955735e8a1828805307c36cfe838aa779844f
Marc Lundgren <marclundgren2.0@gmail.com>
2019-10-24 14:16:31 -0700

    sending messages to chute and storing requests in a postgres database now works with a sqs via docker

--- app/sqs-send-message.js
+++ app/sqs-send-message.js
@@ -1,38 +1,34 @@
 /* eslint-disable no-console */
-require('dotenv').config();
-
 const hash = require('../util/hash');
 const AWS = require('aws-sdk');
+require('dotenv').config();
 
 const {
+  AWS_API_VERSION = '2012-11-05',
+  AWS_REGION,
   AWS_SQS_ACCESS_KEY_ID,
   AWS_SQS_SECRET_ACCESS_KEY,
-  AWS_REGION,
-  AWS_SQS_QUEUE_URL,
-  // NODE_ENV
+  AWS_SQS_QUEUE_URL
 } = process.env;
 
-const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+if (AWS_SQS_ACCESS_KEY_ID) {
+  AWS.config.update({ accessKeyId: AWS_SQS_ACCESS_KEY_ID });
+}
+if (AWS_SQS_SECRET_ACCESS_KEY) {
+  AWS.config.update({ secretAccessKey: AWS_SQS_SECRET_ACCESS_KEY });
+}
+if (AWS_REGION) {
+  AWS.config.update({ region: AWS_REGION });
+}
+
+const sqs = new AWS.SQS({ apiVersion: AWS_API_VERSION });
 
 const {
   DelaySeconds = 0,
-  MessageBody = JSON.stringify({ account_id: null, account_name: null, franchisor_id: 8, franchisor_name: 'Bean Me Up - Irvine', photo_id: 93, photo_tags: 'coffee,tea,bakery,wifi,study', photo_url: 'https://res.cloudinary.com/ralliohq/bmuc02gx9og4tcdqghcb.jpg'}),
+  MessageBody = JSON.stringify({account_id: null, account_name: null, franchisor_id: 8, franchisor_name: "Bean Me Up - SoCal", photo_id: 93, photo_tags: "animal,mammal,pet,furniture,dog", photo_url: "https://res.cloudinary.com/ralliohq/q_auto/s7swm1swodxajmli0uhf.jpg"}),
   QueueUrl = AWS_SQS_QUEUE_URL,
-  region = AWS_REGION,
-  accessKeyId = AWS_SQS_ACCESS_KEY_ID,
-  secretAccessKey = AWS_SQS_SECRET_ACCESS_KEY,
   numberOfMessagesToSend = 1
 } = hash(process.argv);
-
-if (accessKeyId) {
-  AWS.config.update({ accessKeyId });
-}
-if (secretAccessKey) {
-  AWS.config.update({ secretAccessKey });
-}
-if (region) {
-  AWS.config.update({ region });
-}
 
 let promises = [];
 promises.length = numberOfMessagesToSend;
@@ -40,15 +36,26 @@ promises.fill();
 
 const messagePromises = promises.map(() => {
   return new Promise((resolve, reject) => {
-    sqs.sendMessage({ DelaySeconds, MessageBody, QueueUrl }, (err, response) => {
+    const handleSendMessage = (err, response) => {
+
       if (err) {
         console.error(err);
-        reject(err);
-      } else {
-        console.table(response);
-        resolve(response);
+        return reject(err);
       }
-    });
+
+      resolve(response);
+    };
+
+    try {
+      sqs.sendMessage({
+        DelaySeconds,
+        MessageBody,
+        QueueUrl
+      }, handleSendMessage);
+    } catch (err) {
+      console.error(err);
+      reject(err);
+    }
   });
 });
 
