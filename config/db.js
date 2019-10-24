146cc6d0fd454a540fb569d5535b799f50e15102
Marc Lundgren <marclundgren2.0@gmail.com>
2019-10-24 16:40:48 -0700

    putting the db in docker and running a test

--- config/db.js
+++ config/db.js
@@ -2,23 +2,18 @@ require('dotenv').config()
 const dialect = 'postgres';
 
 module.exports = {
-  docker: {
+  development: {
     username: 'postgres',
     database: 'chute_development',
     host: '127.0.0.1',
     port: 5433,
     dialect
   },
-  development: {
-    username: 'postgres',
-    database: 'chute_development',
-    host: '127.0.0.1',
-    dialect
-  },
   test: {
     username: 'postgres',
     database: 'chute_test',
     host: '127.0.0.1',
+    port: 5434,
     dialect
   },
   staging: {
