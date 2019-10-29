3b2bbd9a295909e5e3ae7242b3a0639636a51357
Marc Lundgren <marclundgren2.0@gmail.com>
2019-10-29 11:54:47 -0700

    util/create-queue.js includes test coverage

---
+++ __tests__/create-queue.js
@@ -0,0 +1,24 @@
+const {createQueue} = require('../util/sqs/create-queue')
+
+const endpoint = 'http://127.0.0.1:9324'
+
+describe('sanity', () => {
+  it('should import a function', () => {
+    expect(typeof createQueue).toBe('function')
+  })
+
+  it('should return an error when missing a QueueName parameter', async () => {
+    let response = await createQueue({ endpoint }).catch(err => err)
+
+    expect(response).toEqual('missing required QueueName')
+  })
+
+  it('should return a QueueURL when including a QueueName', async () => {
+    let QueueName = 'hello-sqs'
+
+    let response = await createQueue({ endpoint, QueueName }).catch(err => err)
+    expect(typeof response).toBe('object')
+    expect(typeof response.QueueUrl).toBe('string')
+    expect(response.QueueUrl).toEqual(`${endpoint}/queue/${QueueName}`)
+  })
+})
