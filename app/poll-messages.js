a9c9b692ee8431e7849b471cf897d28d36b7cb7c
Marc Lundgren <marc@rallio.com>
2019-11-05 11:02:11 -0800

    processedMessagesCount is not defined

--- app/poll-messages.js
+++ app/poll-messages.js
@@ -45,76 +45,7 @@ const pollMessages = ({
 
   await Promise.all(messageRemovedPromises).catch(console.error); // TODO AWS error handle
 
-  processedMessagesCount += messageRemovedPromises.length;
-
   return messageRemovedPromises.length;
 });
-
-// start();
 
 module.exports = { pollMessages };
-/*
-const {handleMessages} = require('./handle-messages')
-const {longPoller} = require('./long-poller')
-const {removeFromSqs} = require('../util/remove-from-sqs')
-const pollMessages = ({
-    poller = longPoller,
-    remove = removeFromSqs,
-    handle = handleMessages
-} = {}) => poller().then(async (response = {}) => {
-
-  const {
-    Messages
-  } = response;
-
-  if (!Messages) {
-    return 0;
-  }
-
-  let mappedMessages;
-  try {
-    mappedMessages = Messages.map((m) => {
-      const {
-        ReceiptHandle,
-        MessageId
-      } = m;
-
-      return {
-        ...JSON.parse(m.Body),
-        ReceiptHandle,
-        MessageId
-      };
-    });
-  } catch (e) {
-    
-    console.error('no body', e);
-    return Infinity;
-  }
-  if (!mappedMessages) {
-    console.error('no messages');
-    return 0;
-  }
-
-  const messagesProcessed = await handle({messages: mappedMessages});
-
-  if (!messagesProcessed) {
-    return Infinity;
-  }
-  
-  const messageRemovedPromises = messagesProcessed.map(message => {
-    return remove({
-      ReceiptHandle: message.ReceiptHandle
-    });
-  });
-  
-  await Promise.all(messageRemovedPromises).catch(console.error); // TODO AWS error handle
-
-//   processedMessagesCount += messageRemovedPromises.length;
-
-  return messageRemovedPromises.length;
-});
-
-
-
-module.exports = { pollMessages }
-*/
